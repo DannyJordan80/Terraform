4c4ffeb98bd4c05218720e98cbb214e2a565fe08
danny-jordan <dannyjordan80@gmail.com>
2020-06-05 21:25:04 +0100

    Adding output variables.

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 resource "aws_instance" "example" {
   ami = "ami-0eff4f2497a2ce392"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Welcome!\nTesting Terraform deployment." > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
   tags = {
     Name = "terraform_example"
   }
